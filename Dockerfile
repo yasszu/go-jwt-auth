f6917f6539fb6ea14e4dd58b56c9e9d71e6accf4
Yaushiro Suzuki <yasszu@yahoo.com>
2021-07-10 16:58:21 +0900

    Wait DB connection established

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,12 @@ FROM golang:1.16
 
 WORKDIR /go/src/app
 
-RUN go get -u github.com/cosmtrek/air
+RUN apt-get update && \
+    apt-get -y install postgresql
+
+RUN go get -u github.com/cosmtrek/air && \
+    go get -v github.com/rubenv/sql-migrate/...
 
 EXPOSE 8888
 
-CMD ["air"]
+CMD while ! pg_isready --host=$POSTGRES_HOST --port=$POSTGRES_PORT; do sleep 1; done && air
--- infrastructure/db/db.go
+++ infrastructure/db/db.go
@@ -1,32 +1,15 @@
 package db
 
 import (
-	"fmt"
 	"go-jwt-auth/util/conf"
-	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
-const (
-	retryTimes  = 20
-	waitingTime = 3 * time.Second
-)
-
 func NewConn() (*gorm.DB, error) {
-	db, err := openDB(conf.Postgres.DSN())
-	for i := 0; i < retryTimes; i++ {
-		if err == nil {
-			break
-		}
-		fmt.Println("Waiting for getting the connection of postgres...")
-		time.Sleep(waitingTime)
-		db, err = openDB(conf.Postgres.DSN())
-	}
-
-	return db, err
+	return openDB(conf.Postgres.DSN())
 }
 
 func openDB(dsn string) (*gorm.DB, error) {
