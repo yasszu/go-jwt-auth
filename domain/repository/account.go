4b872b007afe987582a0dfbc46202dcc658ee48d
Yaushiro Suzuki <yasszu@yahoo.com>
2022-06-18 22:38:52 +0900

    generate mock/account.go

--- domain/repository/account.go
+++ domain/repository/account.go
@@ -2,6 +2,7 @@ package repository
 
 import "github.com/yasszu/go-jwt-auth/domain/entity"
 
+//go:generate mockgen -source=./account.go -destination=./mock/account.go -package=mock
 type AccountRepository interface {
 	GetAccountByEmail(email string) (*entity.Account, error)
 	GetAccountByID(accountID uint) (*entity.Account, error)
