15392369511a400c5db7218a54ae6df33987c2a6
Yaushiro Suzuki <yasszu@yahoo.com>
2022-06-19 16:33:37 +0900

    refactor form

--- interfaces/form/account.go
+++ interfaces/form/account.go
@@ -3,8 +3,6 @@ package form
 import (
 	"github.com/yasszu/go-jwt-auth/domain/entity"
 	"github.com/yasszu/go-jwt-auth/util/crypt"
-
-	_validate "github.com/go-playground/validator/v10"
 )
 
 type Signup struct {
@@ -14,7 +12,6 @@ type Signup struct {
 }
 
 func (f *Signup) Validate() error {
-	validate := _validate.New()
 	if err := validate.Struct(f); err != nil {
 		return err
 	}
@@ -40,7 +37,6 @@ type Login struct {
 }
 
 func (f *Login) Validate() error {
-	validate := _validate.New()
 	if err := validate.Struct(f); err != nil {
 		return err
 	}
---
+++ interfaces/form/form.go
@@ -0,0 +1,11 @@
+package form
+
+import "github.com/go-playground/validator/v10"
+
+var (
+	validate *validator.Validate
+)
+
+func init() {
+	validate = validator.New()
+}
