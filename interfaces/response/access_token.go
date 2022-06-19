8569923217adce45eccb1038fbe9d461791da132
Yaushiro Suzuki <yasszu@yahoo.com>
2022-06-19 14:43:44 +0900

    change AccessToken response

--- interfaces/response/access_token.go
+++ interfaces/response/access_token.go
@@ -1,19 +1,21 @@
 package response
 
 import (
+	"time"
+
 	"github.com/yasszu/go-jwt-auth/domain/entity"
 )
 
 type AccessToken struct {
 	AccountID uint   `json:"account_id"`
 	Token     string `json:"access_token"`
-	ExpiresAt int64  `json:"expired_at"`
+	ExpiresAt string `json:"expires_at"`
 }
 
 func NewAccessToken(e *entity.AccessToken) AccessToken {
 	return AccessToken{
 		AccountID: e.AccountID,
 		Token:     e.Token,
-		ExpiresAt: e.ExpiresAt.Unix(),
+		ExpiresAt: e.ExpiresAt.Format(time.RFC3339),
 	}
 }
