ca0843f11c9e42584988346dcfe439b7cd1b6f68
Yaushiro Suzuki <yasszu@yahoo.com>
2022-10-22 17:41:14 +0900

    refactor router

--- interfaces/router/router.go
+++ interfaces/router/router.go
@@ -8,18 +8,16 @@ import (
 
 func NewRouter(h *handler.Handler) *mux.Router {
 	r := mux.NewRouter()
+	r.Use(middleware.Logging)
+	r.Use(middleware.CORS)
 
-	root := r.PathPrefix("").Subrouter()
-	root.Use(middleware.Logging)
-	root.Use(middleware.CORS)
-	root.HandleFunc("/", h.Index).Methods("GET")
-	root.HandleFunc("/healthy", h.Healthy).Methods("GET")
-	root.HandleFunc("/ready", h.Ready).Methods("GET")
-	root.HandleFunc("/signup", h.Signup).Methods("POST")
-	root.HandleFunc("/login", h.Login).Methods("POST")
+	r.HandleFunc("/", h.Index).Methods("GET")
+	r.HandleFunc("/healthy", h.Healthy).Methods("GET")
+	r.HandleFunc("/ready", h.Ready).Methods("GET")
+	r.HandleFunc("/signup", h.Signup).Methods("POST")
+	r.HandleFunc("/login", h.Login).Methods("POST")
 
 	v1 := r.PathPrefix("/v1").Subrouter()
-	v1.Use(middleware.Logging)
 	v1.Use(h.JWT())
 	v1.HandleFunc("/me", h.Me).Methods("GET")
 	return r
